15cd1067404183e3443dd6737b25d6be9133766f
Ben <845687624@qq.com>
2016-10-07 17:33:17 +0800

    Add new method to get content

--- app/src/main/java/com/example/ben/project/News.java
+++ app/src/main/java/com/example/ben/project/News.java
@@ -1,5 +1,14 @@
 package com.example.ben.project;
 
+import android.content.Context;
+import android.content.res.Resources;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+
 /**
  * Created by 84568 on 2016/10/7.
  */
@@ -12,26 +21,14 @@ public class News {
     private String title;
     private int size_of_text;
     private int content_id;
-    public News(int image_id,int content_id){
+    public News(String name_of_provider,String profile_of_provider,int number_of_likes,int number_of_comment,int image_id,String title,int size_of_text,int content_id){
         this.image_id=image_id;
         this.content_id=content_id;
-    }
-    public void setNameOfProvider(String name_of_provider){
         this.name_of_provider=name_of_provider;
-    }
-    public void setProfile_of_provider(String profile_of_provider){
         this.profile_of_provider=profile_of_provider;
-    }
-    public void setNumber_of_likes(int number_of_likes){
         this.number_of_likes=number_of_likes;
-    }
-    public void setNumber_of_comment(int number_of_comment){
         this.number_of_comment=number_of_comment;
-    }
-    public void setTitleOfText(String title){
         this.title=title;
-    }
-    public void setSize_of_text(int size_of_text){
         this.size_of_text=size_of_text;
     }
     public String getNameOfProvider(){
@@ -57,5 +54,28 @@ public class News {
     }
     public int getContent_id(){
         return content_id;
+    }
+    public String getContentString(Context context){
+        Resources res = context.getResources();
+        InputStream in = null;
+        InputStreamReader inputStreamReader = null;
+        try {
+            in = res.openRawResource(content_id);
+            inputStreamReader = new InputStreamReader(in, "gbk");
+        } catch (UnsupportedEncodingException e1) {
+            e1.printStackTrace();
+        }
+        BufferedReader reader = new BufferedReader(inputStreamReader);
+        StringBuffer sb = new StringBuffer("");
+        String line;
+        try {
+            while ((line = reader.readLine()) != null) {
+                sb.append(line);
+                sb.append("\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return sb.toString();
     }
 }
