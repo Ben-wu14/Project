a3d15da878272e9baff903cebefc4c83765bd881
Ben <845687624@qq.com>
2016-10-07 17:47:44 +0800

    Add infor for the adapter

--- app/src/main/java/com/example/ben/project/NewsAdapter.java
+++ app/src/main/java/com/example/ben/project/NewsAdapter.java
@@ -1,9 +1,13 @@
 package com.example.ben.project;
 
 import android.app.Activity;
+import android.text.Layout;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -17,6 +21,19 @@ public class NewsAdapter extends ArrayAdapter<News> {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return super.getView(position, convertView, parent);
+        View listItem =convertView;
+        if(listItem==null){
+            listItem= LayoutInflater.from(getContext()).inflate(R.layout.list_item,parent,false);
+        }
+        News news=getItem(position);
+        TextView topicText=(TextView)listItem.findViewById(R.id.topic_title);
+        topicText.setText(news.getTitleOfText());
+        TextView size=(TextView)listItem.findViewById(R.id.size_of_info);
+        size.setText(news.getSize_of_text());
+        TextView content=(TextView)listItem.findViewById(R.id.preview_of_content);
+        content.setText(news.getContentString(getContext()));
+        ImageView image=(ImageView)listItem.findViewById(R.id.icon_for_people);
+        image.setImageResource(news.getImage_id());
+        return listItem;
     }
 }
