a87c7977b590a7d42f40c4be9ea0562a31e883df
Ben <845687624@qq.com>
2016-10-07 19:11:03 +0800

    Add a new activity

---
+++ app/src/main/java/com/example/ben/project/ContentActivity.java
@@ -0,0 +1,14 @@
+package com.example.ben.project;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class ContentActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_content);
+    }
+
+}
